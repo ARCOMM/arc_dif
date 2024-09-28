fa5f363b374d7ded305ea4459af396ce0307bd68
tim4242 <puzzleddev@gmail.com>
2024-09-28 12:46:37 +0200

    Update to HEMTT 1.13.0

---
+++ mod.cpp
@@ -0,0 +1,7 @@
+name = "ARCOMM DIF";
+actionName = "Website";
+action = "https://arcomm.co.uk";
+description = "";
+tooltip = "ARC Difficulty";
+tooltipOwned = "ARC Difficulty Owned";
+author = "ARCOMM";
